7a2f518869ed58a5630f1e14c1bb23855dc9bb59
guidoambiorix <guidoambiorix@gmail.com>
2023-04-20 20:04:02 -0400

    --se creo el crud de personas

---
+++ CrudBAckend/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudBAckend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudBAckend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+
+    }
+}
---
+++ CrudBAckend/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudBAckend.Models
+{
+    [Table("Persona")]
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+
+    }
+}
